9ad30577f0e556f8695f88bff35f95393510b91d
Jakob Pollok <jakob.pollok@student.uni-tuebingen.de>
2026-04-22 12:55:50 +0200

    More work in vllm

--- HF_AssistantModel.py
+++ HF_AssistantModel.py
@@ -1,9 +1,9 @@
 import torch
 import time
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 
-model = "Qwen/Qwen3-8B"
-draft_model_name  = "Qwen/Qwen3-1.7B"
+model = "Qwen/Qwen3-32B"
+draft_model_name  = "Qwen/Qwen3-8B"
 
 # Rn this is using the same tokenizer for both models. Both from Qwen 3, so probably the same. Might be an issue with other models tho
 tokenizer = AutoTokenizer.from_pretrained(model)
@@ -20,7 +20,10 @@ specdec_model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
 )
 
-inputs = tokenizer("Tell me what color the sky is.", return_tensors="pt").to("cuda")
+inputs = tokenizer("Tell me how to groom a dog.", return_tensors="pt").to("cuda")
+
+
+# TODO: Add warmup for better testing/benching
 
 # Normal model output
 torch.cuda.synchronize()
@@ -28,31 +31,45 @@ start = time.time()
 
 out = og_model.generate(
     **inputs,
-    max_new_tokens=100,
+    max_new_tokens=500,
+    min_new_tokens=500,
     do_sample=True,
-    temperature=0.7
+    temperature=0.7,
+    use_cache=True
 )
 
 torch.cuda.synchronize()
 end = time.time()
+og_time = end - start
 
+print()
 print("OG Model:  ", end - start)
-
+print(tokenizer.decode(out[0]))
+print()
 
 # SpecDec output
 torch.cuda.synchronize()
 start = time.time()
 
-out = specdec_model.generate(
+out = og_model.generate(
     **inputs,
-    max_new_tokens=100,
+    assistant_model=specdec_model,
+    max_new_tokens=500,
+    min_new_tokens=500,
     do_sample=True,
     temperature=0.7,
-    assistant_model=specdec_model,
-    assistant_tokenizer=tokenizer #Same for both models rn
+    use_cache=True,
+    num_assistant_tokens = 4
+    # assistant_tokenizer=tokenizer Same for both models rn so this line is not really needed
 )
 
 torch.cuda.synchronize()
 end = time.time()
 
-print("SpecDec Model:  ", end - start)
+specdec_time = end - start
+print()
+print("SpecDec Model:  ", specdec_time)
+print(tokenizer.decode(out[0]))
+print()
+
+print("We had a speedup of: ", og_time/specdec_time)
--- vllm_DraftModel.py
+++ vllm_DraftModel.py
@@ -1,24 +1,26 @@
 from vllm import LLM, SamplingParams
 import time
 
-og_model = LLM(model ="Qwen/Qwen3-8B")
+prompts = ["What color is the sky?", "A dog is pet known for?", "How does one make pizza?"]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-specDec_models = LLM(
-    model = "Qwen/Qwen3-8B",
-    speculative_model = "Qwen/Qwen3-1.7B"
-)
-
-parameters = SamplingParams(
-    max_tokens = 100,
-    temperature=0.7
+llm = LLM(
+    model="Qwen/Qwen3-8B",
+    tensor_parallel_size=4,
+    speculative_config={
+        "model": "Qwen/Qwen3-0.6B",
+        "num_speculative_tokens": 5,        
+        "method": "draft_model",
+},
 )
 
 start = time.time()
-out = og_model.generate("Write a story about a robot", parameters)
+outputs = specdec_model.generate(prompts, sampling_params)
 end = time.time()
-print("OG Model took:  ", end-start)
 
-start = time.time()
-out = specDec_models.generate("Write a story about a robot", parameters)
-end = time.time()
-print("SpecDec Model took:  ", end-start)
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+print("Model took:  ", end-start)
